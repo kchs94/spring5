858f27804b227ba5417ac9a586965db13e7de0ad
kchs94 <kchs94@gmail.com>
2021-12-09 23:38:10 +0900

    First Commit

---
+++ src/test/java/com/tistory/kchs94/spring5/Spring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.kchs94.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
